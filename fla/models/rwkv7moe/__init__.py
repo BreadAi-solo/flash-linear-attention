f8fa4ddfc9c010a7ed732b961a6fae50c630d6f8
breadbrowser <64813014+breadbrowser@users.noreply.github.com>
2025-02-11 16:52:27 -0500

    Update __init__.py

--- fla/models/rwkv7moe/__init__.py
+++ fla/models/rwkv7moe/__init__.py
@@ -2,12 +2,12 @@
 
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
 
-from fla.models.rwkv7moe.configuration_rwkv7moe import RWKV7MOEConfig
+from fla.models.rwkv7moe.configuration_rwkv7moe import rwkv7MoeConfig
 from fla.models.rwkv7moe.modeling_rwkv7moe import RWKV7MOEForCausalLM, RWKV7MOEModel
 
-AutoConfig.register(RWKV7MOEConfig.model_type, RWKV7MOEConfig, True)
-AutoModel.register(RWKV7MOEConfig, RWKV7MOEModel, True)
-AutoModelForCausalLM.register(RWKV7MOEConfig, RWKV7MOEForCausalLM, True)
+AutoConfig.register(rwkv7MoeConfig.model_type, RWKV7MOEConfig, True)
+AutoModel.register(rwkv7MoeConfig, RWKV7MOEModel, True)
+AutoModelForCausalLM.register(rwkv7MoeConfig, RWKV7MOEForCausalLM, True)
 
 
-__all__ = ['RWKV7MOEConfig', 'RWKV7MOEForCausalLM', 'RWKV7MOEModel']
+__all__ = ['rwkv7MoeConfig', 'RWKV7MOEForCausalLM', 'RWKV7MOEModel']
